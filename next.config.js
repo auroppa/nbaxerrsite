49f9b76ecb16570fce4e07536a8c95b98500fe08
Buğra Durgut <bugradurgutt@gmail.com>
2025-05-17 18:28:33 +0300

    Update GitHub Pages configuration and build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/nbaxerrsite',
   images: {
     unoptimized: true,
   },
+  assetPrefix: '/nbaxerrsite/',
 }
 
 module.exports = nextConfig 
