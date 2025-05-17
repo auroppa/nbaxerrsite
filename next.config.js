cc7e2a98caaf4f2091e6e4e65b420fdb193b8b58
Buğra Durgut <bugradurgutt@gmail.com>
2025-05-17 19:21:31 +0300

    refactor: simplify build process and configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
   basePath: '/nbaxerrsite',
   assetPrefix: '/nbaxerrsite',
+  images: { unoptimized: true },
+  distDir: 'dist',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
